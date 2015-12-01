5bdbfb64c99be4dd569cf19a101d7b04a677eefc
bodhi <bodhi@5263.org>
2015-12-01 13:01:42 +1100

    Remove placeholder survey info

--- frontend/src/components/Feedback.js
+++ frontend/src/components/Feedback.js
@@ -15,19 +15,6 @@ function Feedback({ emailToPatient, emailTo, surveyScore, dailyScore, age, gende
             <FeedbackCharts surveyScore={surveyScore} dailyScore={dailyScore} age={age} gender={gender} />
             <Prompt surveyScore={surveyScore} dailyScore={dailyScore} age={age} gender={gender} />
 
-            <p>Looking at this information, how do you feel about your level of drinking?</p>
-
-            <p>Only you can make the decision to change your consumption. Here are some strategies you can try if you want to cut back:</p>
-
-            <ul>
-            <li>Setting personal drinking limits and sticking to it;</li>
-            <li>Alternating alcoholic drinks with soft drinks;</li>
-            <li>Switching to low alcohol drinks;</li>
-            <li>Having regular alcohol-free days;</li>
-            <li>Identifying high risk situations for heavy drinking and creating a management plan;</li>
-            <li>Engaging in alternative activities to drinking</li>
-            </ul>
-
             <div id='section-buttons'>
             <button className='mdl-button mdl-button--raised mdl-button--colored' onClick={emailToPatient}>Email to Patient</button>
             <button className='mdl-button mdl-button--raised mdl-button--colored'  onClick={emailTo}>Email to…</button>
