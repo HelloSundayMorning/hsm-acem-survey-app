644e2aa02623903708e20228a8c14ed0e9eebe6f
bodhi <bodhi@5263.org>
2016-01-20 15:22:15 +1100

    Convert FramesReminder component to stateless function

--- frontend/src/components/FramesReminder.js
+++ frontend/src/components/FramesReminder.js
@@ -1,43 +1,38 @@
 import React from 'react'
 
 
-export default React.createClass({
-    render: function() {
-        return (
-            <section id='reminder'>
-                <h1>FRAMES reminder</h1>
+export default () =>
+    <section id='reminder'>
+        <h1>FRAMES reminder</h1>
 
-                <h2>Feedback</h2>
-                <p>Show the previous screen to the patient. Many people are unaware that they are drinking at hazardous or harmful levels and highlighting risks linked to current drinking patterns can be a powerful motivator for change.</p>
+        <h2>Feedback</h2>
+        <p>Show the previous screen to the patient. Many people are unaware that they are drinking at hazardous or harmful levels and highlighting risks linked to current drinking patterns can be a powerful motivator for change.</p>
 
-                <p>“You’ve scored 16 on the AUDIT which indicates that you are at high risk of harm from your current pattern of drinking…”</p>
+        <p>“You’ve scored 16 on the AUDIT which indicates that you are at high risk of harm from your current pattern of drinking…”</p>
 
-                <h2>Responsibility</h2>
-                <p>Emphasise that the decision to change drinking patterns or to continue drinking at the same level is the choice of the person alone.</p>
-                <p>“Nobody can make this choice for you. It’s really up to you to make a change…”</p>
+        <h2>Responsibility</h2>
+        <p>Emphasise that the decision to change drinking patterns or to continue drinking at the same level is the choice of the person alone.</p>
+        <p>“Nobody can make this choice for you. It’s really up to you to make a change…”</p>
 
-                <h2>Advice</h2>
-                <p>Deliver clear advice to change drinking behaviour.</p>
-                <p>“…yet as your (doctor, pharmacist, health care worker) I strongly advise you to limit your drinking or stop altogether to reduce the risks.”</p>
+        <h2>Advice</h2>
+        <p>Deliver clear advice to change drinking behaviour.</p>
+        <p>“…yet as your (doctor, pharmacist, health care worker) I strongly advise you to limit your drinking or stop altogether to reduce the risks.”</p>
 
-                <h2>Provide a menu of strategies for changing drinking behaviours.</h2>
-                <p>Examples:</p>
+        <h2>Provide a menu of strategies for changing drinking behaviours.</h2>
+        <p>Examples:</p>
 
-                <ul>
-                    <li>Setting personal drinking limits and sticking to it;</li>
-                    <li>Alternating alcoholic drinks with soft drinks;</li>
-                    <li>Switching to low alcohol drinks;</li>
-                    <li>Having regular alcohol-free days;</li>
-                    <li>Identifying high risk situations for heavy drinking and creating a management plan; Engaging in alternative activities to drinking.</li>
-                </ul>
+        <ul>
+            <li>Setting personal drinking limits and sticking to it;</li>
+            <li>Alternating alcoholic drinks with soft drinks;</li>
+            <li>Switching to low alcohol drinks;</li>
+            <li>Having regular alcohol-free days;</li>
+            <li>Identifying high risk situations for heavy drinking and creating a management plan; Engaging in alternative activities to drinking.</li>
+        </ul>
 
-                <h2>Empathy</h2>
-                <p>Emphasise the development of a therapeutic alliance in the context of a warm, reflective, empathic, and collaborative approach by the practitioner.</p>
+        <h2>Empathy</h2>
+        <p>Emphasise the development of a therapeutic alliance in the context of a warm, reflective, empathic, and collaborative approach by the practitioner.</p>
 
-                <h2>Self-efficacy</h2>
-                <p>Support the person&#8217;s self-efficacy for change, and communicate a sense of optimism. De-emphasise helplessness or powerlessness.</p>
-                <p>“Many people successfully control their drinking or stop drinking all together. With the right support and information I’m confident that you will do it too”.</p>
-            </section>
-        );
-    }
-});
+        <h2>Self-efficacy</h2>
+        <p>Support the person&#8217;s self-efficacy for change, and communicate a sense of optimism. De-emphasise helplessness or powerlessness.</p>
+        <p>“Many people successfully control their drinking or stop drinking all together. With the right support and information I’m confident that you will do it too”.</p>
+    </section>;
