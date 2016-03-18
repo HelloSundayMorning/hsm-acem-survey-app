4fb04b33c585a9a4e6ad053c1d6a99b90e0359b8
bodhi <bodhi@5263.org>
2016-03-18 15:49:49 +1100

    Rework reducers to reduce code duplication

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -8,39 +8,33 @@ import updateSurvey from 'reducers/survey'
 import routeChanged from 'reducers/routeChanged'
 import bio from 'reducers/bio'
 
-const interviewer = (s = null, a) => {
-    if (a.type === actions.SET_INTERVIEWER) {
-        return a.value
+// Returns handler(s, a) when `a.type` is in `actions`, otherwise
+// state is unchanged
+const when = handler => (...actions) => (s = null, a) => {
+    if (actions.indexOf(a.type) >= 0) {
+        return handler(s, a)
     }
     return s
 }
 
-const location = (s = null, a) => {
-    if (a.type === actions.UPDATE_LOCATION) {
-        return a.value
-    }
-    return s
-}
+// Reducer combinators that accepts a list of actions, and will return
+// the value or type of the action when action matches the passed
+// types.
+const actionValue = when((_, a) => a.value);
+const actionType = when((_, a) => a.type);
 
-const postingEmail = (s = null, a) => {
-    switch (a.type) {
-    case actions.EMAIL_SENDING:
-    case actions.EMAIL_SENT:
-    case actions.EMAIL_FAILED:
-        return a.type
-    }
-    return s
-}
-
-const postingSurvey = (s = null, a) => {
-    switch (a.type) {
-    case actions.POSTING_SURVEY:
-    case actions.SURVEY_POSTED:
-    case actions.SURVEY_POST_FAILED:
-        return a.type
-    }
-    return s
-}
+const interviewer = actionValue(actions.SET_INTERVIEWER);
+const location = actionValue(actions.UPDATE_LOCATION);
+const postingEmail = actionType(
+    actions.EMAIL_SENDING,
+    actions.EMAIL_SENT,
+    actions.EMAIL_FAILED
+);
+const postingSurvey = actionType(
+    actions.POSTING_SURVEY,
+    actions.SURVEY_POSTED,
+    actions.SURVEY_POST_FAILED
+);
 
 const id = (a = null) => a
 
