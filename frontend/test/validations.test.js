981fb2c79a15507c092fde34a8e4eaf7da5aa324
Hui <junhuif@gmail.com>
2016-03-24 07:44:10 +0800

    Refactor to clean up validations unit tests
    1. Rename valid to expected
    2. Put all testCases in a same definition.

--- frontend/test/validations.test.js
+++ frontend/test/validations.test.js
@@ -12,8 +12,8 @@ const runTestCases = (testCasesByName, testMethod) => {
             const { label, testCases } = testCasesByName[name];
             it(label, () => {
                 for (let index in testCases) {
-                    const { input, valid } = testCases[index];
-                    expect(testMethod(name, input)).to.equal(valid);
+                    const { input, expected } = testCases[index];
+                    expect(testMethod(name, input)).to.equal(expected);
                 }
             });
         }
@@ -22,50 +22,50 @@ const runTestCases = (testCasesByName, testMethod) => {
 
 const noValidationPage = page => ({
     label: `no validation in ${JSON.stringify(page)} page`,
-    testCases: [{ input: {}, valid: true }]
+    testCases: [{ input: {}, expected: true }]
 });
 
 const pages = {
     '/': {
         label: 'interviewer must be present in "/" page',
         testCases: [
-            { input: {}, valid: false },
-            { input: { interviewer: null }, valid: false },
-            { input: { interviewer: 'nurse' }, valid: true }
+            { input: {}, expected: false },
+            { input: { interviewer: null }, expected: false },
+            { input: { interviewer: 'nurse' }, expected: true }
         ]
     },
     'info': {
         label: 'bio required fields must be present and valid in "info" page',
         testCases: [
-            { input: { bio: {} }, valid: false },
-            { input: { bio: { 'age': 12 } }, valid: false },
-            { input: { bio: { 'age': 12, 'gender': 'male' } }, valid: false },
-            { input: { bio: { 'age': 12, 'gender': 'male', 'postcode': '310001' } }, valid: true },
-            { input: { bio: { 'email': 'person@email.com' } }, valid: false }
+            { input: { bio: {} }, expected: false },
+            { input: { bio: { 'age': 12 } }, expected: false },
+            { input: { bio: { 'age': 12, 'gender': 'male' } }, expected: false },
+            { input: { bio: { 'age': 12, 'gender': 'male', 'postcode': '310001' } }, expected: true },
+            { input: { bio: { 'email': 'person@email.com' } }, expected: false }
         ]
     },
     'audit': {
         label: 'every questions must be answered in "audit" page',
         testCases: [
-            { input: { survey:[], lastQuestion: 0 }, valid: false },
-            { input: { survey:[{ question: 'Q', answer: null }], lastQuestion: 0 }, valid: false },
-            { input: { survey:[{ question: 'Q', answer: 'A' }], lastQuestion: 0 }, valid: true },
-            { input: { survey:[{ question: 'Q', answer: 'A' }], lastQuestion: 1 }, valid: false }
+            { input: { survey:[], lastQuestion: 0 }, expected: false },
+            { input: { survey:[{ question: 'Q', answer: null }], lastQuestion: 0 }, expected: false },
+            { input: { survey:[{ question: 'Q', answer: 'A' }], lastQuestion: 0 }, expected: true },
+            { input: { survey:[{ question: 'Q', answer: 'A' }], lastQuestion: 1 }, expected: false }
         ]
-    }
+    },
+    'feedback': noValidationPage('feedback'),
+    'frames': noValidationPage('frames')
 };
-pages['feedback'] = noValidationPage('feedback');
-pages['frames'] = noValidationPage('frames');
 
 runTestCases(pages, validatePage);
 
 const genericField = (field, value, required) => ({
     label: required ? `${field} must be present` : `${field} is optional`,
     testCases: [
-        { input: value, valid: true },
-        { input: '', valid: !required },
-        { input: null, valid: !required },
-        { input: undefined, valid: !required }
+        { input: value, expected: true },
+        { input: '', expected: !required },
+        { input: null, expected: !required },
+        { input: undefined, expected: !required }
     ]
 });
 const optionalField = (field, value) => genericField(field, value, false);
@@ -75,19 +75,19 @@ const bioFields = {
     'age': {
         label: 'age must between 12 and 110',
         testCases: [
-            { input: 12, valid: true },
-            { input: 65, valid: true },
-            { input: 110, valid: true },
-            { input: 11, valid: false },
-            { input: 11, valid: false },
-            { input: 111, valid: false }
+            { input: 12, expected: true },
+            { input: 65, expected: true },
+            { input: 110, expected: true },
+            { input: 11, expected: false },
+            { input: 11, expected: false },
+            { input: 111, expected: false }
         ]
-    }
+    },
+    'gender': requiredField('gender', 'male'),
+    'postcode': requiredField('postcode', '310001'),
+    'email': optionalField('email', 'person@example.com'),
+    'phone': optionalField('phone', '+86020-84429400'),
+    'unknown': optionalField('unknown', 'whatever')
 };
-bioFields['gender'] = requiredField('gender', 'male');
-bioFields['postcode'] = requiredField('postcode', '310001');
-bioFields['email'] = optionalField('email', 'person@example.com');
-bioFields['phone'] = optionalField('phone', '+86020-84429400');
-bioFields['unknown'] = optionalField('unknown', 'whatever');
 
 runTestCases(bioFields, validateBioField);
