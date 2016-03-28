3e21b8515000da662fbb8b12ecef6000d03642d7
Hui <junhuif@gmail.com>
2016-03-28 17:37:57 +0800

    Setup crontab job for sending invitation mail every day
    1. Setup dev environment in order to check deploy scripts and crontab
    script.
    2. Update envrc to add new variables for crontab
    3. Support send-invitation-mail flag for running task

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
+	"github.com/theplant/hsm-acem-survey-app/app/models"
 	"github.com/theplant/hsm-acem-survey-app/config/routes"
 
 	// For DB Automigration
@@ -17,6 +19,14 @@ func die(err error) {
 }
 
 func main() {
+	var sendInvitation bool
+	flag.BoolVar(&sendInvitation, "send-invitation-mail", false, "Send Invitation Mail for surveys and exit")
+	flag.Parse()
+	if sendInvitation {
+		models.SendInvitationMails()
+		return
+	}
+
 	mux := routes.Mux()
 	die(http.ListenAndServe(":14000", mux))
 }
